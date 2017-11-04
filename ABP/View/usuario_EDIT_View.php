33e293ee7e328312bbf1d540074cca11ac546ead
Adrián <asfarinhas@esei.uvigo.es>
2017-11-04 18:52:54 +0100

    diseño formulario edit usuarios cambiado

--- ABP/View/usuario_EDIT_View.php
+++ ABP/View/usuario_EDIT_View.php
@@ -36,15 +36,62 @@
                     <div class="card-body">    
                 <form name = 'Form' action = '../Controller/usuario_Controller.php' method = 'post' onsubmit = 'comprobar_usuario()'>
 
-                    <?php echo $strings['dni'] ?> : <input type = 'text' name = 'dni' size = '10' value ='<?php echo ($this->valores['dni']); ?>' required  readonly  onblur="esVacio(this)  && comprobarText(this, 10)" ><br>
-        <?php echo $strings['nombre'] ?> : <input type = 'text' name = 'nombre' size = '30' value ='<?php echo ($this->valores['nombre']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 30)" ><br>
-        <?php echo $strings['apellidos'] ?> : <input type = 'text' name = 'apellidos' size = '30' value ='<?php echo ($this->valores['apellidos']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 30)" ><br>
-        <?php echo $strings['edad'] ?> : <input type = 'number' name = 'edad' min = '' max = '' value ='<?php echo ($this->valores['edad']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 4)" ><br>
-        <?php echo $strings['contrasena'] ?> : <input type = 'text' name = 'contraseña' size = '30' value ='<?php echo ($this->valores['contrasena']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 30)" ><br>
-        <?php echo $strings['email'] ?> : <input type = 'text' name = 'email' size = '100' value ='<?php echo ($this->valores['email']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 100)" ><br>
-        <?php echo $strings['telefono'] ?> : <input type = 'text' name = 'telefono' size = '20' value ='<?php echo ($this->valores['telefono']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 20)" ><br>
-        <?php echo $strings['fechaAlta'] ?> : <input readonly class = 'tcal' type = 'date' name = 'fechaAlta' min = '' max = '' value ='<?php echo ($this->valores['fechaAlta']); ?>' ></br>
-        
+                    <div class="form-group">
+                        <div class="form-row">
+                            <div class="col-md-6">
+                                <label for="nombre"><?php echo $strings['dni'] ?> : </label>
+                                <input class="form-control" type = 'text' name = 'dni' size = '10' value ='<?php echo ($this->valores['dni']); ?>' required  readonly  onblur="esVacio(this)  && comprobarText(this, 10)" >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                                <label for="nombre"><?php echo $strings['nombre'] ?> : </label>
+                                <input class="form-control" type = 'text' name = 'nombre' size = '30' value ='<?php echo ($this->valores['nombre']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 30)" >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                                    <label for="nombre"><?php echo $strings['edad'] ?> : </label>
+                                    <input class="form-control" type = 'text' name = 'edad' min = '' max = '' value ='<?php echo ($this->valores['edad']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 4)" >
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                                    <label for="nombre"><?php echo $strings['contrasena'] ?> : </label>
+                                     <input class="form-control" type = 'text' name = 'contraseña' size = '30' value ='<?php echo ($this->valores['contrasena']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 30)" >
+                                      </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                        <label for="nombre"><?php echo $strings['email'] ?> : </label>
+                        <input class="form-control" type = 'text' name = 'email' size = '100' value ='<?php echo ($this->valores['email']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 100)" >
+                           </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                        <label for="nombre"><?php echo $strings['telefono'] ?> : </label>
+                        <input class="form-control" type = 'text' name = 'telefono' size = '20' value ='<?php echo ($this->valores['telefono']); ?>' required  onblur="esVacio(this)  && comprobarText(this, 20)" >
+                        </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                                <div class="form-row">
+                                <div class="col-md-6">
+                                    <label for="nombre"><?php echo $strings['fechaAlta'] ?> : </label>
+                                    <input readonly class = 'tcal' type = 'text' name = 'fechaAlta' min = '' max = '' value ='<?php echo ($this->valores['fechaAlta']); ?>' >
+                                    </div>
+                            </div>
+                        </div>
                 </form>
                 <button type="button" onclick="window.location.href='../Controller/usuario_Controller.php?action=default'" class="btn btn-default"><?php echo $strings['Volver']; ?></button> 
             <button type='submit' name='action' form="form1" value='EDIT' class="btn btn-primary"><?php echo $strings['Modificar']; ?></button> 
