e63417cee29fd2975c260912091e211e54b58c97
avsixto <33360198+avsixto@users.noreply.github.com>
2017-11-14 18:07:57 +0100

    Eliminador Controller Álex
    Eliminados controladores inecesarios, solo hace falta un controlador para todos los tipos de ejercicios.

--- ABP/controller/EjercicioController.php
+++ ABP/controller/EjercicioController.php
@@ -1,123 +1,52 @@
+<?php
 
-<?php 
-    session_start(); //solicito trabajar con la session
+require_once(__DIR__."/../core/ViewManager.php");
+require_once(__DIR__."/../controller/BaseController.php");
+require_once(__DIR__."/../model/EjercicioMapper.php");
+require_once(__DIR__."/../model/EjercicioEstiramientoMapper.php");
+require_once(__DIR__."/../model/EjercicioMuscularMapper.php");
+require_once(__DIR__."/../model/EjercicioCardioMapper.php");
 
-    include '../Model/ejercicio_Model.php';
+class EjercicioController extends BaseController{
 
-    include '../View/MESSAGE_View.php';
-    include '../Functions/Authentication.php';
+	private $UsuarioMapper;
 
-    if (!IsAuthenticated()){
-        header('Location:../index.php');
+	public function __construct() {
+		parent::__construct();/*llama al contructor padre 'BaseController de gestion de la sesion*/
+		$this->UsuarioMapper = new UsuarioMapper();
+	}
+	
+    public function loadView() {/*Carga la vista de login para entrar en la web*/
+        require_once("view/usuario/login.php");
     }
-    include '../Locates/Strings_'.$_SESSION['idioma'].'.php';
 
-    /*Generamos los includes de las diferentes vistas*/
-    include '../View/ejercicio_ADD_View.php';
-    include '../View/ejercicio_DELETE_View.php';
-    include '../View/ejercicio_EDIT_View.php';
-    include '../View/ejercicio_SEARCH_View.php';
-    include '../View/ejercicio_SHOWCURRENT_View.php';
-    include '../View/ejercicio_SHOWALL_View.php';
-
-    function get_data_form(){
-
-    //Recoge la información del formulario
-
-                $idEjercicio = $_REQUEST['idEjercicio'];
-
-         
-                $nombre = $_REQUEST['nombre'];
-
-         
-                $descripcion = $_REQUEST['descripcion'];
-
-         
-                $video = $_REQUEST['video'];
-
-         
-                $imagen = $_REQUEST['imagen'];
-
-         $accion = $_REQUEST['action'];
-
-    $ejercicio = new ejercicio_Model($idEjercicio,$nombre,$descripcion,$video,$imagen);
-
-    return $ejercicio;
-}
-
-if (!isset($_REQUEST['action'])){
-    $_REQUEST['action'] = '';
-}
-    
-    /*A continuación creamos el switch con el cual podremos gestionar las peticiones de ADD, DELETE, EDIT...*/
-    switch ($_REQUEST['action']) {
-        /*Caso añadir a la BD*/
-        case 'ADD': 
-                if (!$_POST){
-                    new ejercicio_ADD();
-                }
-                else{
-                    $ejercicio = get_data_form();
-                    $respuesta = $ejercicio->ADD();
-                    new MESSAGE($respuesta, '../Controller/ejercicio_Controller.php');
-                }
-                break;      
-        case 'DELETE': //Borrado de actividades
-           if (!$_POST){
-                    $ejercicio = new ejercicio_Model($_REQUEST['idEjercicio'],'','','','');
-                    $valores = $ejercicio->RellenaDatos();
-                    new ejercicio_DELETE($valores);
-                }
-                else{
-                    $ejercicio = get_data_form();
-                    $respuesta = $ejercicio->DELETE();
-                    new MESSAGE($respuesta, '../Controller/ejercicio_Controller.php');
-                }
-                break;
-        case 'SHOWCURRENT': //Mostrar información detallada
-                $ejercicio = new ejercicio_Model($_REQUEST['idEjercicio'],'','','','');
-                $valores = $ejercicio->RellenaDatos();
-                new ejercicio_SHOWCURRENT($valores);
-                break;
-        case 'EDIT': //Modificación de actividades
-if (!$_POST){
-                    $ejercicio = new ejercicio_Model($_REQUEST['idEjercicio'],'','','','');
-                    $valores = $ejercicio->RellenaDatos();
-                    new ejercicio_EDIT($valores);
-                }
-                else{
-                    
-                    $ejercicio = get_data_form();
-
-                    $respuesta = $ejercicio->EDIT();
-                    new MESSAGE($respuesta, '../Controller/ejercicio_Controller.php');
-                }
-                
-                break;
-        case 'SEARCH': //Consulta de actividades
-            if (!$_POST){
-                    new ejercicio_SEARCH();
-                }
-                else{
-                    $ejercicio = get_data_form();
-                    $datos = $ejercicio->SEARCH();
-                     $lista = array('idEjercicio','nombre','descripcion','video','imagen');
-                    new ejercicio_SHOWALL($lista, $datos, '../index.php');
-                }
-                break;
-        default:
-           if (!$_POST){
-                    $ejercicio = new ejercicio_Model('','','','','');
-                }
-                else{
-                    $ejercicio = get_data_form();
-                }
-                $datos = $ejercicio->SEARCH();
-                $lista = array('idEjercicio','nombre','descripcion','video','imagen');
-                new ejercicio_SHOWALL($lista, $datos,'../Controller/ejercicio_Controller.php' );
-
+    /*Inicia sesión
+    *Si el usuario existe incia sesion y carga la plantilla por defecto
+    *si no muestra un mensaje de error y refresca la vista
+    */
+    public function login(){
+    	/*HABRIA QUE DEFERENCIAR ENTRE ENTRAR COMO DEPORTISTA Y ENTRENADOR/ADMINISTRADOR TIENEN MENUS DIFERENTES*/
+        if(isset($_POST["dni"]) && isset($_POST["contraseña"])){
+            if($this->UsuarioMapper->login($_POST["dni"],$_POST["contraseña"])){
+            /* y se inicia la sesion y se carga la vista por defecto*/
+            $_SESSION["currentuser"]=$_POST["dni"];
+            // send user to the restricted area (HTTP 302 code)
+            $this->view->redirect("main", "index");
+            }else{
+                /*se pone un mensaje de error y se reresca la vista*/
+                $errors["username"] = "Username and/or password not exists in system";
+                $this->view->setFlash($errors["username"]);
             }
-
-    
-
+        }
+    	require_once("view/usuario/login.php");
+    }
+    /*Cierra la sesion */
+    public function logout() {
+		session_destroy();
+		// perform a redirection. More or less:
+		// header("Location: logIn.php?controller=users&action=users")
+		// die();
+		$this->view->redirect("Login", "login");
+	}
+}
 ?>
