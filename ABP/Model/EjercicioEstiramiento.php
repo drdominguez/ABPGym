01d8f1f6fd63317620fcb5a489a882d80fe3c797
avsixto <33360198+avsixto@users.noreply.github.com>
2017-11-14 16:22:46 +0100

    Create EjercicioEstiramiento.php

---
+++ ABP/Model/EjercicioEstiramiento.php
@@ -0,0 +1,35 @@
+<?php
+require_once(__DIR__."/Ejercicio.php");
+class EjercicioEstiramiento extends Ejercicio
+{
+	Private $idEjercicio;
+    Private $tiempo;
+    Private $unidad;
+    Private $distancia;
+        function __construct($idEjercicio=NULL, $nombre=NULL, $descripcion=NUKL, $video=NULL, $imagen=NULL, $tiempo=NULL,$unidad=NULL, $distancia=NULL){
+        	parent::__construct($idEjercicio, $nombre, $descripcion, $video, $imagen);//lamada al contructor padre
+        	$this->idEjercicio = $idEjercicio;
+        	$this->tiempo = $tiempo;
+        	$this->unidad = $unidad;
+        	$this->distancia = $distancia;
+	}
+	public function getTiempo(){
+		return $this->tiempo;
+	}
+	public function getUnidad(){
+		return $this->unidad;
+	}
+	public function getDistancia(){
+		return $this->distancia;
+	}
+	public function setTiempo($tiempo){
+		$this->tiempo=$tiempo;
+	}
+	public function setUnidad($unidad){
+		$this->unidad=$unidad;
+	}
+	public function setDistancia($distancia){
+		$this->distancia=$distancia;
+	}
+}//fin de clase
+?> 
