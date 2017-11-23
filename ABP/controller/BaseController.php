242df199da1635af47565309c6c9e90ec63aca41
Daniel Rodríguez Domínguez <drdominguez@esei.uvigo.es>
2017-11-23 16:07:02 +0100

    vista de grupo añadir arreglada

--- ABP/controller/BaseController.php
+++ ABP/controller/BaseController.php
@@ -1,18 +1,9 @@
 <?php
-//file: controller/BaseController.php
 
 require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../core/I18n.php");
 require_once(__DIR__ . "/../model/Usuario.php");
 
-/**
- * Class BaseController
- *
- * Implements a basic super constructor for
- * the controllers in the Blog App.
- * Basically, it provides some protected
- * attributes and view variables.
- */
 class BaseController {
 
 	/**
