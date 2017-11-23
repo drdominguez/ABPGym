7a38a5bccb0855c37e518ef7a2bfe585e6f35a51
avsixto <33360198+avsixto@users.noreply.github.com>
2017-11-23 02:26:14 +0100

    Boton Realizar Ejercicio

--- ABP/View/sesionEntrenamiento/tablaSHOWALL.php
+++ ABP/View/sesionEntrenamiento/tablaSHOWALL.php
@@ -50,7 +50,7 @@ require_once(__DIR__."/../../core/ViewManager.php");
                                         </a>
                                     </td>
                                     <td>
-                                        <a href='./index.php?controller=Tabla&amp;action=TablaEDIT&amp;idTabla=<?php echo $tabla->getIdTabla();?>'><img src='./view/Icons/edit.png'>
+                                        <a href='./index.php?controller=Tabla&amp;action=TablaEDIT&amp;idTabla=<?php echo $tabla->getIdTabla();?>'><img src='./view/Icons/play.png'>
                                         </a>
                                     </td>
                                 </tr>
