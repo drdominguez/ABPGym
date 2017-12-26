b8ad8dea730446a838a03b74d78e2d1a3228fdcb
Adrián <asfarinhas@esei.uvigo.es>
2017-12-26 01:23:28 +0100

    ah pues no, pero ahora si

--- ABP/view/layouts/Header.php
+++ ABP/view/layouts/Header.php
@@ -1,18 +1,4 @@
 <meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/> 
-
-<?php
-/*  include_once '../../Functions/Authentication.php';
-  if(isset($_SESSION['lang'])){
-        if(strcmp($_SESSION['lang'],'ENGLISH')==0)
-            include("../../Locates/Strings_ENGLISH.php"); 
-        else if(strcmp($_SESSION['lang'],'SPANISH')==0)
-            include("../../Locates/Strings_SPANISH.php");
-        else if(strcmp($_SESSION['lang'], 'GALICIAN')==0)
-        include("../../Locates/Strings_GALICIAN.php"); 
-    }else{
-        include("../../Locates/Strings_GALICIAN.php"); 
-    }*/
-?>
     <html>
 
     <head>
