0739af542fc251c4e4f1bb17f34481113df4927e
avsixto <33360198+avsixto@users.noreply.github.com>
2018-01-11 19:36:12 +0100

    Ver tabla desde inicio sesiones
    se listan las tablas del ususario y puede verlas en detalle con los ejercicios de cada tipo que tiene asignados la tabla

--- ABP/controller/SesionEntrenamientoController.php
+++ ABP/controller/SesionEntrenamientoController.php
@@ -9,14 +9,17 @@ require_once(__DIR__ . "/../model/EjercicioMapper.php");
 require_once(__DIR__ . "/../model/EjercicioCardioMapper.php");
 require_once(__DIR__ . "/../model/EjercicioMuscularMapper.php");
 require_once(__DIR__ . "/../model/EjercicioEstiramientoMapper.php");
+require_once(__DIR__ . "/../core/permisos.php");
 
 class SesionEntrenamientoController extends BaseController{
 
     private $tablaMapper;
+    private $permisos;
 
     public function __construct() {
         parent::__construct();/*llama al contructor padre 'BaseController de gestion de la sesion*/
         $this->tablaMapper = new TablaMapper();
+        $this->permisos= new Permisos();
     }
 
     public function TablaListar(){
@@ -26,24 +29,39 @@ class SesionEntrenamientoController extends BaseController{
         $this->view->render("sesionEntrenamiento","tablaSHOWALL");
     }
 
-    public function TablaView() {
-        $tablaMapper = new TablaMapper();
-        if (!isset($_GET["idTabla"])){
+    public function TablaView() 
+    {
+        if (!isset($_GET["idTabla"])) 
+        {
             throw new Exception("El id es obligatorio");
         }
+        
         $idTabla = $_GET["idTabla"];
-        // find the notification object in the database
-        $tabla =  $tablaMapper->findTablaById($idTabla);
-        $ejercicios = $tablaMapper->findEjerciciosById($idTabla);
 
-        if ($tabla == NULL){
+        // find the notification object in the database
+        $tabla = $this->tablaMapper->findTablaById($idTabla);
+        $musculares = $this->tablaMapper->findMuscularesById($idTabla);
+        $cardios = $this->tablaMapper->findCardiosById($idTabla);
+        $estiramientos = $this->tablaMapper->findEstiramientosById($idTabla);
+        $usuarios = $this->tablaMapper->listarUsuariosTabla($idTabla);
+        $tipoUsuario = $this->permisos->comprobarTipo();
+
+        if ($tabla == NULL) 
+        {
             throw new Exception("No existe tabla con este id: ".$idTabla);
         }
         // put the notification object to the view
-        $this->view->setVariable("tabla", $tabla);
-        $this->view->setVariable("ejercicios", $ejercicios);
+
+        
+        $this->view->setVariable("tipoUsuario",$tipoUsuario);
+        $this->view->setVariable("tabla", $tabla);            
+        $this->view->setVariable("cardios",$cardios);
+        $this->view->setVariable("usuarios",$usuarios);
+        $this->view->setVariable("musculares",$musculares);
+        $this->view->setVariable("estiramientos",$estiramientos);
+
         // render the view (/view/posts/view.php)
-        $this->view->render("sesionEntrenamiento", "tablaSHOWCURRENT");
+        $this->view->render("tabla", "tablaSHOWCURRENT2");
     }
 
     /*
