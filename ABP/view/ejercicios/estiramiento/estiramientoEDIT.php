174bee9bc223b732213d34e8f14a2b72f02d7bac
Adrián <asfarinhas@esei.uvigo.es>
2018-01-15 19:30:51 +0100

    validaciones ejercicios terminada

--- ABP/view/ejercicios/estiramiento/estiramientoEDIT.php
+++ ABP/view/ejercicios/estiramiento/estiramientoEDIT.php
@@ -16,13 +16,13 @@
                     <div class="card-header">
                         <i class="fa fa-table"></i>Editar Estiramiento</div>
                     <div class="card-body">
-                    <form name='Form' id="form1" class="form-signin" action="index.php?controller=Ejercicio&amp;action=estiramientoEdit" enctype="multipart/form-data" accept-charset="UTF-8" method="POST">
-                        <input class="form-control" name="idEjercicio" id="exampleInputIdEjercicio" type="hidden" aria-describedby="emailHelp" value="<?php echo $estiramiento->getIdEjercicio() ?>" readonly="readonly" onblur="esVacio(this)  && comprobarText(this,15)">
+                    <form name='Form' id="form1" class="form-signin" action="index.php?controller=Ejercicio&amp;action=estiramientoEdit" enctype="multipart/form-data" accept-charset="UTF-8" onsubmit="return validarEstiramientoEDIT();" method="POST">
+                        <input class="form-control" name="idEjercicio" id="exampleInputIdEjercicio" type="hidden" aria-describedby="emailHelp" value="<?php echo $estiramiento->getIdEjercicio() ?>" readonly="readonly" >
                         <div class="form-group">
                             <div class="form-row">
                               <div class="col-md-6">
                             <label for="exampleInputNombre">Nombre</label>
-                            <input class="form-control" name="nombre" id="exampleInputNombre" type="TEXT" aria-describedby="emailHelp" value="<?php echo $estiramiento->getNombre() ?>" onblur="esVacio(this)  && comprobarText(this,15)">
+                            <input class="form-control" name="nombre" id="exampleInputNombre" type="TEXT" aria-describedby="emailHelp" value="<?php echo $estiramiento->getNombre() ?>" onblur="comprobarVacio(this)  && comprobarTexto(this,50)">
                             </div>
                             <div class="col-md-6">
                             <label for="exampleInputTiempo">Descripción</label>
@@ -34,7 +34,7 @@
                              <div class="form-row">
                                  <div class="col-md-6">
                                     <label for="exampleInputTiempo">Video</label>
-                                    <input class="form-control" name="video" id="exampleInputVideo" type="url" aria-describedby="emailHelp" placeholder="Video" value="https://www.youtube.com/embed/" onblur="esVacio(this)  && comprobarText(this,15)">
+                                    <input class="form-control" name="video" id="exampleInputVideo" type="url" aria-describedby="emailHelp" placeholder="Video" value="https://www.youtube.com/embed/" >
                                 </div>
                                  <div class="col-md-6">
                                     <iframe width="280" height="155" allowfullscreen frameborder="0" src="<?php echo $estiramiento->getVideo(); ?>"></iframe>
--- ABP/view/ejercicios/muscular/muscularEDIT.php
+++ ABP/view/ejercicios/muscular/muscularEDIT.php
@@ -16,13 +16,13 @@
                     <div class="card-header">
                         <i class="fa fa-table"></i>Editar Muscular</div>
                     <div class="card-body">
-                    <form name='Form' id="form1" class="form-signin" action="index.php?controller=Ejercicio&amp;action=muscularEdit" accept-charset="UTF-8" method="POST">
-                        <input class="form-control" name="idEjercicio" id="exampleInputIdEjercicio" type="hidden" aria-describedby="emailHelp" value="<?php echo $muscular->getIdEjercicio() ?>" readonly="readonly" onblur="esVacio(this)  && comprobarText(this,15)">
+                    <form name='Form' id="form1" class="form-signin" action="index.php?controller=Ejercicio&amp;action=muscularEdit" accept-charset="UTF-8" onsubmit="return validarMuscularEDIT();" method="POST">
+                        <input class="form-control" name="idEjercicio" id="exampleInputIdEjercicio" type="hidden" aria-describedby="emailHelp" value="<?php echo $muscular->getIdEjercicio() ?>" readonly="readonly" >
                         <div class="form-group">
                             <div class="form-row">
                             <div class="col-md-6">
                                 <label for="exampleInputNombre">Nombre</label>
-                                <input class="form-control" name="nombre" id="exampleInputNombre" type="TEXT" aria-describedby="emailHelp" value="<?php echo $muscular->getNombre() ?>" onblur="esVacio(this)  && comprobarText(this,15)">
+                                <input class="form-control" name="nombre" id="exampleInputNombre" type="TEXT" aria-describedby="emailHelp" value="<?php echo $muscular->getNombre() ?>" onblur="comprobarVacio(this)  && comprobarText(this,50)">
                             </div>
                             <div class="col-md-6">
                                 <label for="exampleInputTiempo">Descripción</label>
@@ -34,7 +34,7 @@
                              <div class="form-row">
                                  <div class="col-md-6">
                                     <label for="exampleInputTiempo">Video</label>
-                                    <input class="form-control" name="video" id="exampleInputVideo" type="url" aria-describedby="emailHelp" placeholder="Video" value="https://www.youtube.com/embed/" onblur="esVacio(this)  && comprobarText(this,15)">
+                                    <input class="form-control" name="video" id="exampleInputVideo" type="url" aria-describedby="emailHelp" placeholder="Video" value="https://www.youtube.com/embed/" >
                                 </div>
                                  <div class="col-md-6">
                                      <iframe width="280" height="155" allowfullscreen frameborder="0" src="<?php echo $muscular->getVideo(); ?>"></iframe>
