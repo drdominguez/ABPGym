dc893e0d5ecd40a9cf25b60f38fc7fac30f30b4d
Adrián <asfarinhas@esei.uvigo.es>
2018-01-16 20:51:08 +0100

    menu ahora es fijo

--- ABP/view/layouts/menuLateral-ADMIN.php
+++ ABP/view/layouts/menuLateral-ADMIN.php
@@ -174,15 +174,6 @@
                     </li>
                     
                 </ul>
-                <ul class="navbar-nav sidenav-toggler">
-                    <li class="nav-item">
-                        <a class="nav-link text-center" id="sidenavToggler">
-                            <i class="fa fa-fw fa-angle-left"></i>
-                        </a>
-                    </li>
-                </ul>
-
-            
                 <ul class="navbar-nav ml-auto"> 
                 
         
