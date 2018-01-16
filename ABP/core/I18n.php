3eadab4c8d0c85bdd4ef9a5c195f0729a75fba40
Adrián <asfarinhas@esei.uvigo.es>
2018-01-16 20:48:49 +0100

    Mapa localizacion

--- ABP/core/I18n.php
+++ ABP/core/I18n.php
@@ -4,7 +4,7 @@ class I18n {
 
 	private $messages;
 
-	const DEFAULT_LANGUAGE="es";
+	const DEFAULT_LANGUAGE="gal";
 	const CURRENT_LANGUAGE_SESSION_VAR="__currentlang__";
 
 	public function __construct(){
--- ABP/view/localizacion/localizacionSHOWCURRENT.php
+++ ABP/view/localizacion/localizacionSHOWCURRENT.php
@@ -4,7 +4,12 @@ require_once(__DIR__."/../../core/ViewManager.php");
     $view = ViewManager::getInstance();
 
 ?>
-
+<style>
+       #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
 <div class="content-wrapper">
     <div class="container-fluid">
         <!-- Breadcrumbs-->
@@ -22,7 +27,23 @@ require_once(__DIR__."/../../core/ViewManager.php");
                 <b><?= i18n("Provincia") ?>:</b> <p><?php echo "Ourense"; ?></p>
                 <b><?= i18n("Comunidad Autónoma") ?>:</b> <p><?php echo "Galicia"; ?></p>
                 <b><?= i18n("Pais") ?>:</b><p> <?php echo "España"; ?></p>
-
+                <div id="map"></div>
+                    <script>
+                      function initMap() {
+                        var uluru = {lat: 42.349800, lng: -7.867945};
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                          zoom: 18,
+                          center: uluru
+                        });
+                        var marker = new google.maps.Marker({
+                          position: uluru,
+                          map: map
+                        });
+                      }
+                    </script>
+                    <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsMVGju9TEG4_TayY9J193rGeOdAPI0vI &callback=initMap">
+                    </script>
                 <button type="button" onclick="window.location.href='./index.php?controller=main&action=index'" class="btn btn-primary"><?= i18n("Volver") ?></button> 
             </div>
         </div>
