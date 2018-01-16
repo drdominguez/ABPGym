39b7cb76b50d4e1b27bd84c909238fc56c44c637
Adrián <asfarinhas@esei.uvigo.es>
2018-01-16 20:40:09 +0100

    SIUUUUUUUUUUUUUU

--- ABP/view/localizacion/localizacionSHOWCURRENT.php
+++ ABP/view/localizacion/localizacionSHOWCURRENT.php
@@ -19,12 +19,11 @@ require_once(__DIR__."/../../core/ViewManager.php");
             <div class="card-body">
                 <b><?= i18n("Calle") ?>:</b> <p><?php echo "Avenida de Marín"; ?></p>
                 <b><?= i18n("Número") ?>:</b> <p><?php echo "9"; ?></p>
-                <b><?= i18n("Provicia") ?>:</b> <p><?php echo "Ourense"; ?></p>
-                <b><?= i18n("Número") ?>: </b><p> <?php echo "7"; ?></p> 
+                <b><?= i18n("Provincia") ?>:</b> <p><?php echo "Ourense"; ?></p>
                 <b><?= i18n("Comunidad Autónoma") ?>:</b> <p><?php echo "Galicia"; ?></p>
                 <b><?= i18n("Pais") ?>:</b><p> <?php echo "España"; ?></p>
 
-                <button type="button" onclick="window.location.href='./index.php?controller=Localizacion&amp;action=localizacionListar'" class="btn btn-primary">Volver</button> 
+                <button type="button" onclick="window.location.href='./index.php?controller=main&action=index'" class="btn btn-primary"><?= i18n("Volver") ?></button> 
             </div>
         </div>
     </div>
