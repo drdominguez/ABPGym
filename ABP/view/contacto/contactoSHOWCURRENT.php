1d6c733d93ae9dc5a9952a750a1b0b325c5eb371
Daniel Rodríguez Domínguez <drdominguez@esei.uvigo.es>
2018-01-16 21:15:07 +0100

    KingSoutinho

--- ABP/view/contacto/contactoSHOWCURRENT.php
+++ ABP/view/contacto/contactoSHOWCURRENT.php
@@ -19,7 +19,16 @@ require_once(__DIR__."/../../core/ViewManager.php");
             <div class="card-body">
                 <b><?= i18n("Teléfono atención al cliente") ?>:</b> <p><?php echo "902231267"; ?></p>
                 <b><?= i18n("WhatsApp") ?>:</b> <p><?php echo "623789836"; ?></p>
+                <td>
+                        <a href='www.facebook.com'><span id="icon-face" class="icon-face"></span>
+                        </a>
+                </td>
                 <b><?= i18n("Fcebook") ?>:</b> <p><?php echo "facebook.com/GymApp"; ?></p>
+                <td>
+                        <a href='www.twitter.com'><span id="icon-face" class="icon-face"></span>
+                        </a>
+                </td>
+                
                 <b><?= i18n("Twitter") ?>:</b><p> <?php echo "@GymApp"; ?></p>
                 <b><?= i18n("Correo") ?>: </b><p> <?php echo "atencionGymapp@gmail.com"; ?></p> 
                 
